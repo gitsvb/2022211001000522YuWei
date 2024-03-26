1aca329b157bf20eb6ea3fb3734d62985c1b260d
gitsvb <18827859528@139.com>
2024-03-26 17:34:38 +0800

    Default Changelist

---
+++ src/main/java/Week4homework/ConfigDemoServlet.java
@@ -0,0 +1,32 @@
+package Week4homework;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(
+        urlPatterns = {"/config"},
+        initParams = {
+                @WebInitParam(name="name",value="Yu Wei"),
+                @WebInitParam(name="studentId",value="2022211001000522")
+        },loadOnStartup = 1
+)
+public class ConfigDemoServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ServletConfig config = getServletConfig();
+        String name = config.getInitParameter("name");
+        String id = config.getInitParameter("studentId");
+        PrintWriter writer = response.getWriter();
+        writer.println("name:"+name);
+        writer.println("Student id:"+id);
+        writer.close();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
